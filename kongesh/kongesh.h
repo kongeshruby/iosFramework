5c995fe618b08cbcff8256a8e8c60b67d46cf7a3
kongesh_iOS <kongeshwaran@doodleblue.com>
2019-10-22 16:39:00 +0530

    Initial commit

---
+++ kongesh/kongesh.h
@@ -0,0 +1,19 @@
+//
+//  kongesh.h
+//  kongesh
+//
+//  Created by kongesh on 22/10/19.
+//  Copyright © 2019 kongesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kongesh.
+FOUNDATION_EXPORT double kongeshVersionNumber;
+
+//! Project version string for kongesh.
+FOUNDATION_EXPORT const unsigned char kongeshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kongesh/PublicHeader.h>
+
+
---
+++ kongesh/kongesh.swift
@@ -0,0 +1,21 @@
+//
+//  kongesh.swift
+//  kongesh
+//
+//  Created by kongesh on 22/10/19.
+//  Copyright © 2019 kongesh. All rights reserved.
+//
+
+import UIKit
+
+open class kongesh: NSObject {
+        open class func logToConsole(_ msg: String) {
+            print(msg,msg);
+        }
+    
+    open class func draw(){
+        
+    }
+    
+
+}
